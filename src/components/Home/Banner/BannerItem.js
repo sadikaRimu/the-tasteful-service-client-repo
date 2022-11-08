68675735ca45973bb99f09324fcbfc98563d4e82
sadikaRimu <sadika3046@gmail.com>
2022-11-08 16:10:36 +0600

    initial commit

---
+++ src/components/Home/Banner/BannerItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './BannerItem.css';
+
+const BannerItem = ({ slide }) => {
+    const { image, id, prev, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-image'>
+                <img src={image} alt="" className="w-full rounded-xl" />
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24  top-1/4">
+                <h1 className='text-6xl font-bold text-white'>
+                    Tasteful  <br />
+                    and Yummy Food<br />
+                    Fast Delivery Service
+                </h1>
+            </div>
+            <div className="absolute flex justify-end w-2/5 transform -translate-y-1/2 left-24  top-1/2">
+                <p className='text-white text-xl'>There are many variations of Food items available, you can order your meal from here.</p>
+            </div>
+            <div className="absolute flex justify-start w-2/5 transform -translate-y-1/2 left-24  top-3/4">
+                <button className="btn btn-warning mr-5">Order Now</button>
+                <button className="btn btn-outline btn-warning">Warning</button>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItem;
---
+++ src/components/Home/Services/Services.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+    return (
+        <div>
+            <div className='text-center mb-4'>
+                <p className='text-2xl font-bold text-orange-600'>Services</p>
+                <h2 className="text-5xl font-semibold">Food Menu</h2>
+                <p> There are many variations of Food items available, you can order your meal from here. </p>
+            </div>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
